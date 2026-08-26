2dbd48e2f5567916efb5b0089ba510f56a8f659c
Priyansh Patel <ppriyansh515@gmail.com>
2026-08-26 08:02:58 +0530

    all ready

--- B.java
+++ B.java
@@ -1,11 +1,10 @@
 class B{
-<<<<<<< HEAD
+
   int y = 89;
-=======
-  int y =12;
+
+  int e =12;
 
 void methodB(){
   System.out.println("MEthod B");
-}
->>>>>>> 76c8737640c06912498e8e973c07198cba697039
+  }
 }
